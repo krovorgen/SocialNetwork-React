5ffa5ce870185254ffe93a7534b4ef12d45e34cd
Biarozka <maksim.biarozka@infourok.biz>
2021-06-12 17:40:34 +0300

    Added dialogs

--- src/components/Dialogs/index.tsx
+++ src/components/Dialogs/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-// import styles from './style.module.scss';
+import styles from './style.module.scss';
 
 const Dialogs = () => {
-    return <>Dialogs</>;
+    return (
+        <div className={styles['dialogs']}>
+            <ul className={styles['dialogs__items']}>
+                <li className={styles['dialogs__item']}>Roma</li>
+                <li className={styles['dialogs__item']}>Maxime</li>
+                <li className={styles['dialogs__item']}>Anya</li>
+            </ul>
+            <ul className={styles['messages']}>
+                <li className={styles['messages__item']}>Hallo Welt</li>
+                <li className={styles['messages__item']}>Hallo Welt</li>
+                <li className={styles['messages__item']}>Hallo Welt</li>
+            </ul>
+        </div>
+    );
 };
 
 export default Dialogs;
--- src/components/Test/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import styles from './style.module.scss';
-
-const Test = () => {
-    return <>Test</>;
-};
-
-export default Test;
