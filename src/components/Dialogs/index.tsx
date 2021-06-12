f2c1239ca2bc23db28c024ee6a5678189cd9083c
Biarozka <maksim.biarozka@infourok.biz>
2021-06-12 17:57:44 +0300

    Added NavLink to dialogs

--- src/components/Dialogs/index.tsx
+++ src/components/Dialogs/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import styles from './style.module.scss';
 
@@ -6,9 +7,33 @@ const Dialogs = () => {
     return (
         <div className={styles['dialogs']}>
             <ul className={styles['dialogs__items']}>
-                <li className={styles['dialogs__item']}>Roma</li>
-                <li className={styles['dialogs__item']}>Maxime</li>
-                <li className={styles['dialogs__item']}>Anya</li>
+                <li className={styles['dialogs__item']}>
+                    <NavLink
+                        className={styles['dialogs__link']}
+                        activeClassName={styles['dialogs__link--active']}
+                        to="/dialogs/1"
+                    >
+                        Roma
+                    </NavLink>
+                </li>
+                <li className={styles['dialogs__item']}>
+                    <NavLink
+                        className={styles['dialogs__link']}
+                        activeClassName={styles['dialogs__link--active']}
+                        to="/dialogs/2"
+                    >
+                        Maxime
+                    </NavLink>
+                </li>
+                <li className={styles['dialogs__item']}>
+                    <NavLink
+                        className={styles['dialogs__link']}
+                        activeClassName={styles['dialogs__link--active']}
+                        to="/dialogs/3"
+                    >
+                        Anya
+                    </NavLink>
+                </li>
             </ul>
             <ul className={styles['messages']}>
                 <li className={styles['messages__item']}>Hallo Welt</li>
