1a34596d03b49d26038f0f69638576f431fd5031
Максим Сергеевич <krovorgen@gmail.com>
2021-07-14 00:32:29 +0300

    added StoryBook

---
+++ src/components/Button/index.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Button from './index';
+
+export default {
+  title: 'atoms/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const DefaultButton = Template.bind({});
+DefaultButton.args = {
+  children: 'Click me!',
+};
+
+export const Full = Template.bind({});
+Full.args = {
+  size: 'full',
+  children: 'Click me!',
+};
