4e17a46fa1b8872961ecde162f6858786d98e463
maksim <krovorgen@gmail.com>
2022-01-29 13:21:04 +0300

    файловая структура

--- src/pages/Profile/Posts/PostItem/PostItem.tsx
+++ src/pages/Profile/Posts/PostItem/PostItem.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
 
 import { PostItemType } from '../../../../redux/reducers/types';
+import avatar from '../../../../images/avatar.png';
 
 import styles from '../style.module.scss';
 
 export const PostItem: FC<PostItemType> = ({ message, likesCount, id }) => (
   <li className={styles['posts__item']}>
-    <img className={styles['posts__avatar']} width={50} height={50} src="./images/avatar.png" alt="avatar" />
+    <img className={styles['posts__avatar']} width={50} height={50} src={avatar} alt="avatar" />
     {message}
     <span className={styles['posts__likes']}>Likes: {likesCount}</span>
   </li>
